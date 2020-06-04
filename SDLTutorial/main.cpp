157797158c409f5ac17ce2c583e7f8c5a996b23b
tony <wabslygzj@163.com>
2020-06-04 14:23:30 +0800

    SDL Add pictures on SDL_Surface which belongs to the SDLWindow.

---
+++ SDLTutorial/main.cpp
@@ -0,0 +1,28 @@
+#include  <stdio.h>
+
+extern "C" {
+#include <SDL.h>
+#include <SDL_thread.h>
+};
+
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("SDLÊÓÆµ²¥·ÅÆ÷", 200, 200, 800, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Rect rect;
+	rect.x = 100;
+	rect.y = 100;
+
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+	SDL_Surface* image = SDL_LoadBMP("sample.bmp");
+
+	SDL_BlitSurface(image, NULL, surface, &rect);
+
+	SDL_UpdateWindowSurface(window);
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
